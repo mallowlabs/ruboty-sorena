a111a97432de7773993f771e3b8cc01bce34943b
mallowlabs <mallowlabs@gmail.com>
2015-03-01 18:05:19 +0900

    Added: all: true

--- lib/ruboty/handlers/sorena.rb
+++ lib/ruboty/handlers/sorena.rb
@@ -3,7 +3,7 @@
 module Ruboty
   module Handlers
     class Sorena < Base
-      on /それな|sorena/i, name: "sorena", description: "Request それな"
+      on /それな|sorena/i, name: "sorena", description: "Request それな", all: true
 
       def sorena(message)
         body = %w(
